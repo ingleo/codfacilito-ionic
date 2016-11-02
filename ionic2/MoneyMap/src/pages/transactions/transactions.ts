71f74d828e94137e1d6e7e46b4448d72840dce6a
Leonardo González <clgrhc@gmail.com>
2016-11-02 02:57:56 -0500

    creacion componente
    primer componente para ejemplo movimientos

---
+++ ionic2/MoneyMap/src/pages/transactions/transactions.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Transactions page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-transactions',
+  templateUrl: 'transactions.html'
+})
+export class Transactions {
+
+  title: string = "Movimientos";
+  
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Transactions Page');
+  }
+
+}
